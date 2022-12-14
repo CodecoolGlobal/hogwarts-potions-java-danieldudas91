a26e83fdbd178608f2f48850a3b93b5a56e8e4ba
danieldudas91 <duddaniel0919@gmail.com>
2022-12-14 10:58:19 +0100

    implement DAO methods

--- src/main/java/com/codecool/hogwartshouses/service/DAO/RoomMemory.java
+++ src/main/java/com/codecool/hogwartshouses/service/DAO/RoomMemory.java
@@ -1,10 +1,15 @@
 package com.codecool.hogwartshouses.service.DAO;
 
 import com.codecool.hogwartshouses.model.Room;
+import com.codecool.hogwartshouses.model.Student;
+import com.codecool.hogwartshouses.model.types.PetType;
 import org.springframework.stereotype.Repository;
 
 import java.util.HashSet;
+import java.util.NoSuchElementException;
 import java.util.Set;
+import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Repository
 public class RoomMemory implements RoomDAO {
@@ -24,10 +29,54 @@ public class RoomMemory implements RoomDAO {
         }
     }
 
-    public void addGivenNumberOfRooms(int numberOfRooms){
+    public void addGivenNumberOfRooms(int numberOfRooms, int capacity){
         for (int i = 0; i < numberOfRooms; i++) {
-            Room newRoom = new Room();
+            Room newRoom = new Room(capacity);
             rooms.add(newRoom);
         }
     }
+
+    @Override
+    public void deleteRoom(UUID roomId) {
+        Room roomToFind = rooms.stream().filter(room -> room.getId().equals(roomId)).findFirst().orElse(null);
+        if(roomToFind != null){
+            rooms.remove(roomToFind);
+        }
+        else{
+            System.out.println("No room found with given id!");
+        }
+    }
+
+    @Override
+    public void updateRoom(UUID roomId) {
+
+    }
+
+    @Override
+    public Room findRoomById(UUID roomId) {
+        return rooms.stream().filter(room -> room.getId().equals(roomId)).findFirst().orElseThrow(NoSuchElementException::new);
+    }
+
+    @Override
+    public void assignStudentToRoom(UUID roomId, Student student) {
+        Room foundRoom = rooms.stream().filter(room -> room.getId().equals(roomId)).findFirst().orElse(null);
+        if(foundRoom != null){
+            foundRoom.addStudent(student);
+        }
+        else{
+            System.out.println("No room found with given id!");
+        }
+    }
+
+    @Override
+    public Set<Room> getAllAvailableRooms() {
+        return rooms.stream().filter(Room::isAvailable).collect(Collectors.toSet());
+    }
+
+    @Override
+    public Set<Room> getAvailableRoomsForRatOwners() {
+        return rooms.stream().filter(Room::hasOwlOrCat).collect(Collectors.toSet());
+    }
+
+
 }
