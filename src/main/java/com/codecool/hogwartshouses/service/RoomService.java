90eeb2101c26bd666376aea8374a196fd03e44a7
danieldudas91 <duddaniel0919@gmail.com>
2022-12-14 14:08:54 +0100

    implement all methods needed to connect Room repository to RoomController

--- src/main/java/com/codecool/hogwartshouses/service/RoomService.java
+++ src/main/java/com/codecool/hogwartshouses/service/RoomService.java
@@ -1,8 +1,12 @@
 package com.codecool.hogwartshouses.service;
 
+import com.codecool.hogwartshouses.model.Room;
 import com.codecool.hogwartshouses.service.DAO.RoomDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Set;
+import java.util.UUID;
 
 @Service
 public class RoomService {
@@ -10,4 +14,28 @@ public class RoomService {
     @Autowired
     private RoomDAO roomDAO;
 
+
+    public Set<Room> getAllRooms() {
+        return roomDAO.getAllRooms();
+    }
+
+    public void addNewRoom(int capacity) {
+        roomDAO.addNewRoom(capacity);
+    }
+
+    public Room getRoomById(UUID roomId) {
+        return roomDAO.findRoomById(roomId);
+    }
+
+    public void renovateRoom(UUID roomId) {
+        roomDAO.updateRoom(roomId);
+    }
+
+    public Set<Room> getAvailableRooms() {
+        return roomDAO.getAllAvailableRooms();
+    }
+
+    public Set<Room> getAvailableRoomsForRatOwners() {
+        return roomDAO.getAvailableRoomsForRatOwners();
+    }
 }
