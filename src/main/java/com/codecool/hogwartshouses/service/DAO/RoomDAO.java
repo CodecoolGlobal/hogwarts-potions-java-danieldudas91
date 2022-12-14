02d561c64a28ae60e0f30586f9ec48b5504df674
danieldudas91 <duddaniel0919@gmail.com>
2022-12-14 10:15:23 +0100

    implement RoomDAO methods

--- src/main/java/com/codecool/hogwartshouses/service/DAO/RoomDAO.java
+++ src/main/java/com/codecool/hogwartshouses/service/DAO/RoomDAO.java
@@ -1,5 +1,23 @@
 package com.codecool.hogwartshouses.service.DAO;
 
+import com.codecool.hogwartshouses.model.Room;
+import com.codecool.hogwartshouses.model.types.PetType;
+
+import java.util.Set;
+import java.util.UUID;
+
 public interface RoomDAO {
+    void addRoom(Room room);
+
+    void addGivenNumberOfRooms(int numberOfRooms);
+
+    void deleteRoom(UUID roomId);
+
+    void updateRoom(UUID roomId);
+    Room findRoomById(UUID roomId);
+
+    void assignStudentToRoom(UUID roomId, String studentName);
+    Set<Room> getAllAvailableRooms();
+    Set<Room> getAvailableRoomsForStudentsWithCertainTypeOfPet(PetType petType);
 
 }
