781c3d69db96cfe0ec1f601247acd32cc8f3169a
danieldudas91 <duddaniel0919@gmail.com>
2022-12-14 09:53:32 +0100

    introduce id and students fields in Room class

--- src/main/java/com/codecool/hogwartshouses/model/Room.java
+++ src/main/java/com/codecool/hogwartshouses/model/Room.java
@@ -4,9 +4,15 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
 @Data
 @NoArgsConstructor
 @Builder
 public class Room {
+    private UUID id;
+    private Set<Student> students;
 
 }
