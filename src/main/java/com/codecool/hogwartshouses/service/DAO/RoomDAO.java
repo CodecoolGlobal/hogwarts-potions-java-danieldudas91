dd07e9f3380652a7cb46dae29c84d2d5b932b93a
danieldudas91 <duddaniel0919@gmail.com>
2022-12-14 14:11:32 +0100

    refactor addGivenNumberOfRooms and assignStudentToRoom methods

--- src/main/java/com/codecool/hogwartshouses/service/DAO/RoomDAO.java
+++ src/main/java/com/codecool/hogwartshouses/service/DAO/RoomDAO.java
@@ -2,7 +2,6 @@ package com.codecool.hogwartshouses.service.DAO;
 
 import com.codecool.hogwartshouses.model.Room;
 import com.codecool.hogwartshouses.model.Student;
-import com.codecool.hogwartshouses.model.types.PetType;
 
 import java.util.Set;
 import java.util.UUID;
@@ -10,15 +9,17 @@ import java.util.UUID;
 public interface RoomDAO {
     void addRoom(Room room);
 
-    void addGivenNumberOfRooms(int numberOfRooms, int capacity);
+    void addNewRoom(int capacity);
 
     void deleteRoom(UUID roomId);
 
     void updateRoom(UUID roomId);
     Room findRoomById(UUID roomId);
 
-    void assignStudentToRoom(UUID roomId, Student student);
+    void assignStudentToRoom(Student student);
     Set<Room> getAllAvailableRooms();
     Set<Room> getAvailableRoomsForRatOwners();
+
+    Set<Room> getAllRooms();
 
 }
