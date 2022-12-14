982ed63574588292ae8823477b1b77dd42a16429
danieldudas91 <duddaniel0919@gmail.com>
2022-12-14 14:12:06 +0100

    refactor initialize method

--- src/main/java/com/codecool/hogwartshouses/data_sample/RoomCreator.java
+++ src/main/java/com/codecool/hogwartshouses/data_sample/RoomCreator.java
@@ -4,18 +4,21 @@ import com.codecool.hogwartshouses.service.DAO.RoomMemory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+
 @Component
 public class RoomCreator {
     private static final int NUMBER_OF_ROOMS = 10;
     private static final int MAX_ROOM_CAPACITY = 4;
     @Autowired
     RoomMemory roomMemory;
-
+    @PostConstruct
     public void initialize() {
-        roomMemory.addGivenNumberOfRooms(NUMBER_OF_ROOMS, MAX_ROOM_CAPACITY);
+        for (int i = 0; i < NUMBER_OF_ROOMS; i++) {
+            roomMemory.addNewRoom(MAX_ROOM_CAPACITY);
+        }
     }
 
     public RoomCreator() {
-        initialize();
     }
 }
