bbb638595797f212e6a17ec722f27c50e68b60d3
danieldudas91 <duddaniel0919@gmail.com>
2022-12-14 09:52:44 +0100

    introduce name, age and petType fields in Student class

--- src/main/java/com/codecool/hogwartshouses/model/Student.java
+++ src/main/java/com/codecool/hogwartshouses/model/Student.java
@@ -1,5 +1,6 @@
 package com.codecool.hogwartshouses.model;
 
+import com.codecool.hogwartshouses.model.types.PetType;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,5 +9,10 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Builder
 public class Student {
+
+    private String name;
+    private int age;
+    private PetType petType;
+
 
 }
