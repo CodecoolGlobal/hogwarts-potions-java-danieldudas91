56441f457fb2d19dd3e67ce1631fc815b37edd7b
danieldudas91 <duddaniel0919@gmail.com>
2022-12-15 18:57:47 +0100

    -implement helper methods -refactor naming

--- src/main/java/com/codecool/hogwartshouses/data_sample/StudentCreator.java
+++ src/main/java/com/codecool/hogwartshouses/data_sample/StudentCreator.java
@@ -12,8 +12,12 @@ import java.util.List;
 import java.util.Random;
 @Component
 public class StudentCreator {
-    private static final List<String> STUDENT_NAMES = List.of("Gipsz Jakab", "Kala Pál", "Rónaszéky Ivette",
-            "Ilosvai Arisztid", "Kovács Zoárd", "Szűcs Sándor", "Horváth Ajándék");
+    private static final List<String> FIRST_NAMES = List.of("Jakab", "Pál", "Ivette", "Sándor",
+                                                    "Ajándék", "Oszkár", "Arisztid", "Erzsébet",
+                                                    "Tasziló", "Anna", "Vilmos", "Júlia");
+    private static final List<String> LAST_NAMES = List.of("Gipsz", "Németh", "Rónaszéky",
+                                                    "Ilosvai", "Kovács", "Szűcs", "Horváth",
+                                                    "Nagy", "Kis", "Molnár", "Egerszalóki", "Jónás");
     private static final Random RANDOM = new Random();
     private static final int NUMBER_OF_STUDENTS = 10;
     private static final int MAX_AGE = 18;
@@ -25,12 +29,28 @@ public class StudentCreator {
     }
 
     public Student createRandomStudent(){
-        String randomName = STUDENT_NAMES.get(RANDOM.nextInt(STUDENT_NAMES.size()));
-        PetType randomPetType = PetType.values()[RANDOM.nextInt(PetType.values().length)];
-        int randomAge = RANDOM.nextInt(MAX_AGE - MIN_AGE) + MIN_AGE;
-        HouseType randomHouseType = HouseType.values()[RANDOM.nextInt(HouseType.values().length)];
-        return new Student(randomName, randomAge, randomPetType, randomHouseType);
+        return new Student(generateRandomName(), getRandomAge(), getRandomPetType(), getRandomHouseType());
     }
+
+    private static HouseType getRandomHouseType() {
+        return HouseType.values()[RANDOM.nextInt(HouseType.values().length)];
+    }
+
+    private static int getRandomAge() {
+        return RANDOM.nextInt(MAX_AGE - MIN_AGE) + MIN_AGE;
+    }
+
+    private static PetType getRandomPetType() {
+        return PetType.values()[RANDOM.nextInt(PetType.values().length)];
+    }
+
+    private static String generateRandomName() {
+        String randomFirstName = FIRST_NAMES.get(RANDOM.nextInt(FIRST_NAMES.size()));
+        String randomLastName = LAST_NAMES.get(RANDOM.nextInt(LAST_NAMES.size()));
+        return randomFirstName + " " + randomLastName;
+    }
+
+
     @PostConstruct
     public void initStudents(){
         for (int i = 0; i < NUMBER_OF_STUDENTS; i++) {
