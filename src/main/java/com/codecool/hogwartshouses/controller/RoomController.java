b86857663f84807fc22a49fab6da42962c9ff3a9
danieldudas91 <duddaniel0919@gmail.com>
2022-12-15 09:44:05 +0100

    add CrossOrigin annotation

--- src/main/java/com/codecool/hogwartshouses/controller/RoomController.java
+++ src/main/java/com/codecool/hogwartshouses/controller/RoomController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.Set;
 import java.util.UUID;
-
+@CrossOrigin("*")
 @RestController
 public class RoomController {
     @Autowired
