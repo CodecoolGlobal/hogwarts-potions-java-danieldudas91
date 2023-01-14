94c0f691a08ae56821b8dc99ea359570ebe67149
danieldudas91 <duddaniel0919@gmail.com>
2023-01-14 17:00:57 +0100

    add EnableJpaRepositories annotation

--- src/main/java/com/codecool/hogwartspotions/HogwartsPotionsApplication.java
+++ src/main/java/com/codecool/hogwartspotions/HogwartsPotionsApplication.java
@@ -2,8 +2,10 @@ package com.codecool.hogwartspotions;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories("com.codecool.hogwartspotions.service.JPA")
 public class HogwartsPotionsApplication {
 
 	public static void main(String[] args) {
