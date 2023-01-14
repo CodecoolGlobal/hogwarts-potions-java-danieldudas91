0ffce2ac57ae2f8f3e425d5c9aa2f166f58bb63c
danieldudas91 <duddaniel0919@gmail.com>
2023-01-14 17:00:15 +0100

    add annotations: Entity, NoArgsConstructor, Table, Id, Enumerated

--- src/main/java/com/codecool/hogwartspotions/model/Student.java
+++ src/main/java/com/codecool/hogwartspotions/model/Student.java
@@ -3,16 +3,24 @@ package com.codecool.hogwartspotions.model;
 import com.codecool.hogwartspotions.model.types.HouseType;
 import com.codecool.hogwartspotions.model.types.PetType;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
 import java.util.UUID;
 
 @Data
+@Entity
+@NoArgsConstructor
+@Table(name = "students")
 public class Student {
 
     private String name;
     private int age;
+    @Id
     private UUID id;
+    @Enumerated(EnumType.STRING)
     private PetType petType;
+    @Enumerated(EnumType.STRING)
     private HouseType houseType;
 
     public Student(String name, int age, PetType petType, HouseType houseType) {
