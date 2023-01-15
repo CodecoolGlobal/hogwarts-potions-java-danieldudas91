46914fbea72c33171976cfbfc6243ae543a9bd27
danieldudas91 <duddaniel0919@gmail.com>
2023-01-15 19:35:53 +0100

    refactor initialize method

--- src/main/java/com/codecool/hogwartspotions/data_sample/RoomCreator.java
+++ src/main/java/com/codecool/hogwartspotions/data_sample/RoomCreator.java
@@ -1,28 +1,42 @@
 package com.codecool.hogwartspotions.data_sample;
 
+import com.codecool.hogwartspotions.model.Room;
 import com.codecool.hogwartspotions.model.types.HouseType;
-import com.codecool.hogwartspotions.service.DAO.RoomMemory;
+import com.codecool.hogwartspotions.service.JPA.RoomRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
-import java.util.Random;
 
 @Component
 public class RoomCreator {
-    private static final int NUMBER_OF_ROOMS = 10;
+    private static final int NUMBER_OF_ROOMS = 12;
     private static final int MAX_ROOM_CAPACITY = 4;
-    private static final Random RANDOM = new Random();
     @Autowired
-    RoomMemory roomMemory;
+    RoomRepository roomRepository;
+
     public RoomCreator() {
     }
+
     @PostConstruct
     public void initialize() {
-        for (int i = 0; i < NUMBER_OF_ROOMS; i++) {
-            HouseType randomHouseType = HouseType.values()[RANDOM.nextInt(HouseType.values().length)];
-            roomMemory.addNewRoom(MAX_ROOM_CAPACITY, randomHouseType);
+        if(!roomRepository.findAll().iterator().hasNext()){
+            for (int i = 0; i < NUMBER_OF_ROOMS; i++) {
+                HouseType houseType;
+                if (0 <= i && i < 3) {
+                    houseType = HouseType.GRYFFINDOR;
+                } else if (3 <= i && i < 6) {
+                    houseType = HouseType.HUFFLEPUFF;
+                } else if (6 <= i && i < 9) {
+                    houseType = HouseType.RAVENCLAW;
+                } else {
+                    houseType = HouseType.SLYTHERIN;
+                }
+                String name = houseType.name() + "room";
+                roomRepository.save(new Room(name, MAX_ROOM_CAPACITY, houseType));
+            }
         }
+
     }
 
 }
