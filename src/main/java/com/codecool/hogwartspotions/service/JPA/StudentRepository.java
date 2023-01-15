78b92d2c29a124c35dd3b85c462023cd1014f4ed
danieldudas91 <duddaniel0919@gmail.com>
2023-01-15 19:29:15 +0100

    add findStudentByName method

--- src/main/java/com/codecool/hogwartspotions/service/JPA/StudentRepository.java
+++ src/main/java/com/codecool/hogwartspotions/service/JPA/StudentRepository.java
@@ -6,4 +6,6 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.UUID;
 
 public interface StudentRepository extends CrudRepository<Student, UUID> {
+    Student findStudentByName(String name);
+
 }
