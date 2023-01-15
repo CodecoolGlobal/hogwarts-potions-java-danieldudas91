94e2cf12b63e1e24b09992abb8d271f47604fa7a
danieldudas91 <duddaniel0919@gmail.com>
2023-01-15 19:28:52 +0100

    create RoomRepository interface with findAllByAvailableTrue, findAllByAvailableTrueAndRatFriendlyTrue and findAllByAvailableTrueAndHouseType methods

---
+++ src/main/java/com/codecool/hogwartspotions/service/JPA/RoomRepository.java
@@ -0,0 +1,19 @@
+package com.codecool.hogwartspotions.service.JPA;
+
+import com.codecool.hogwartspotions.model.Room;
+import com.codecool.hogwartspotions.model.types.HouseType;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+
+public interface RoomRepository extends CrudRepository<Room, Long> {
+
+    List<Room> findAllByAvailableTrue();
+    List<Room> findAllByAvailableTrueAndRatFriendlyTrue();
+
+    List<Room> findAllByAvailableTrueAndHouseType(HouseType houseType);
+
+
+
+}
