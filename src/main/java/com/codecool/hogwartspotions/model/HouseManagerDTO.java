e72cda2ada91834b86eb86627a39fc79b8c0620e
danieldudas91 <duddaniel0919@gmail.com>
2023-01-15 19:34:53 +0100

    change roomId type to Long

--- src/main/java/com/codecool/hogwartspotions/model/HouseManagerDTO.java
+++ src/main/java/com/codecool/hogwartspotions/model/HouseManagerDTO.java
@@ -11,7 +11,7 @@ import java.util.UUID;
 @NoArgsConstructor
 public class HouseManagerDTO {
     private int capacity;
-    private UUID roomId;
+    private Long roomId;
     private String studentName;
     private int studentAge;
     private PetType studentPetType;
