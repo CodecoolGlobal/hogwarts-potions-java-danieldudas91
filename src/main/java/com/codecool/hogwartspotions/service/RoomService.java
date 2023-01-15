50201291268f72366c9c3db6a0cc4bdf6e11a6c1
danieldudas91 <duddaniel0919@gmail.com>
2023-01-15 19:33:25 +0100

    -refactor methods to work with RoomRepository and StudentRepository

--- src/main/java/com/codecool/hogwartspotions/service/RoomService.java
+++ src/main/java/com/codecool/hogwartspotions/service/RoomService.java
@@ -1,46 +1,59 @@
 package com.codecool.hogwartspotions.service;
 
 import com.codecool.hogwartspotions.model.Room;
+import com.codecool.hogwartspotions.model.Student;
 import com.codecool.hogwartspotions.model.types.HouseType;
-import com.codecool.hogwartspotions.service.DAO.RoomDAO;
+import com.codecool.hogwartspotions.service.JPA.RoomRepository;
+import com.codecool.hogwartspotions.service.JPA.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.Set;
-import java.util.UUID;
+import java.util.*;
 
 @Service
 public class RoomService {
 
     @Autowired
-    private RoomDAO roomDAO;
+    private RoomRepository roomRepository;
+    @Autowired
+    private StudentRepository studentRepository;
 
 
-    public Set<Room> getAllRooms() {
-        return roomDAO.getAllRooms();
+    public List<Room> getAllRooms() {
+        return (List<Room>) roomRepository.findAll();
     }
 
     public void addNewRoom(int capacity, HouseType houseType) {
-        roomDAO.addNewRoom(capacity, houseType);
+        String name = houseType.name() + "room";
+        roomRepository.save(new Room(name, capacity, houseType));
     }
 
-    public Room getRoomById(UUID roomId) {
-        return roomDAO.findRoomById(roomId);
+    public Room getRoomById(Long roomId) {
+        return roomRepository.findById(roomId).orElseThrow(NoSuchElementException::new);
     }
 
-    public void renovateRoom(UUID roomId) {
-        roomDAO.updateRoom(roomId);
+    public void assignStudentToRoom(Student student) {
+        Room foundRoom = roomRepository.findAllByAvailableTrueAndHouseType(student.getHouseType())
+                .stream()
+                .max(Comparator.comparing(room -> room.getStudents().size()))
+                .orElseThrow(NoSuchElementException::new);
+        foundRoom.addStudent(student);
+        student.setRoomId(foundRoom.getId());
+        roomRepository.save(foundRoom);
+        studentRepository.save(student);
     }
 
-    public Set<Room> getAvailableRooms() {
-        return roomDAO.getAllAvailableRooms();
+    public List<Room> getAvailableRooms() {
+        return roomRepository.findAllByAvailableTrue();
     }
 
-    public Set<Room> getAvailableRoomsForRatOwners() {
-        return roomDAO.getAvailableRoomsForRatOwners();
+    public List<Room> getAvailableRoomsForRatOwners() {
+        return roomRepository.findAllByAvailableTrueAndRatFriendlyTrue();
     }
 
-    public void deleteRoomById(UUID roomId) {
-        roomDAO.deleteRoom(roomId);
+    public void deleteRoomById(Long roomId) {
+        roomRepository.deleteById(roomId);
     }
+
+
 }
--- src/main/java/com/codecool/hogwartspotions/service/StudentService.java
+++ src/main/java/com/codecool/hogwartspotions/service/StudentService.java
@@ -3,31 +3,29 @@ package com.codecool.hogwartspotions.service;
 import com.codecool.hogwartspotions.model.Student;
 import com.codecool.hogwartspotions.service.DAO.RoomDAO;
 import com.codecool.hogwartspotions.service.DAO.StudentDAO;
+import com.codecool.hogwartspotions.service.JPA.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Set;
 
 @Service
 public class StudentService {
+
     @Autowired
-    StudentDAO studentDAO;
-    @Autowired
-    RoomDAO roomDAO;
+    StudentRepository studentRepository;
 
     public void addStudent(Student student) {
-        studentDAO.addStudent(student);
+        studentRepository.save(student);
     }
 
-    public void assignStudentToRoom(Student student) {
-        roomDAO.assignStudentToRoom(student);
-    }
 
-    public Set<Student> getAllStudents() {
-        return studentDAO.getAllStudents();
+    public List<Student> getAllStudents() {
+        return (List<Student>) studentRepository.findAll();
     }
 
     public Student findStudentByName(String name) {
-        return studentDAO.findStudentByName(name);
+        return studentRepository.findStudentByName(name);
     }
 }
