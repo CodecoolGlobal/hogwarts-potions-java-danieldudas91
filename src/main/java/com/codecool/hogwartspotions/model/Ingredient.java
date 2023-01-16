585ec92293b347ac5f5c6ae2fad6c15867c94f4b
danieldudas91 <duddaniel0919@gmail.com>
2023-01-16 20:20:20 +0100

    refactor field for many-to-one relations

--- src/main/java/com/codecool/hogwartspotions/model/Ingredient.java
+++ src/main/java/com/codecool/hogwartspotions/model/Ingredient.java
@@ -1,9 +1,11 @@
 package com.codecool.hogwartspotions.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import java.util.Objects;
 
 @Entity
 @Data
@@ -11,13 +13,29 @@ import javax.persistence.*;
 @Table(name="ingredients")
 public class Ingredient {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(name = "recipe_id")
-    private Long recipeId;
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "recipe_id")
+    @JsonIgnore
+    private Recipe recipe;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "potion_id")
+    @JsonIgnore
+    private Potion potion;
     private String name;
 
     public Ingredient(String name) {
         this.name = name;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Ingredient that = (Ingredient) o;
+        return Objects.equals(name, that.name);
+    }
+
 }
