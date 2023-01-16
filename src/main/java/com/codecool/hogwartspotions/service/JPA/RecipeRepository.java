2d3da7738f61acbd15c1f4101782d0ea1246535e
danieldudas91 <duddaniel0919@gmail.com>
2023-01-16 20:14:56 +0100

    add findAllByStudent method

--- src/main/java/com/codecool/hogwartspotions/service/JPA/RecipeRepository.java
+++ src/main/java/com/codecool/hogwartspotions/service/JPA/RecipeRepository.java
@@ -1,7 +1,11 @@
 package com.codecool.hogwartspotions.service.JPA;
 
 import com.codecool.hogwartspotions.model.Recipe;
+import com.codecool.hogwartspotions.model.Student;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface RecipeRepository extends CrudRepository<Recipe, Long> {
+    List<Recipe> findAllByStudent(Student student);
 }
